36b95277b5b8db9a338c8dd7d2c652fcf9cc65eb
CoelhoBatfix <panoff.2008@gmail.com>
2023-02-15 18:36:21 +0200

    Navbar correction. Tabs place and existence
    Temporarily removed "Home" tab. Replaced "Login" and "Logout" to the right

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -46,11 +46,11 @@ const App: React.FC = () => {
           Dashboard
         </Link>
         <div className="navbar-nav mr-auto">
-          <li className="nav-item">
+         {/*<li className="nav-item">
             <Link to={"/home"} className="nav-link">
               Home
             </Link>
-          </li>
+          </li>*/}
 
           {showAdminBoard && (
             <li className="nav-item">
@@ -76,25 +76,25 @@ const App: React.FC = () => {
                 {currentUser.username}
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item log-in-out">
               <a href="/login" className="nav-link" onClick={logOut}>
                 LogOut
               </a>
             </li>
           </div>
         ) : (
-          <div className="navbar-nav ml-auto">
+          <div className="navbar-nav ml-auto log-in-out">
             <li className="nav-item">
               <Link to={"/login"} className="nav-link">
                 Login
               </Link>
             </li>
 
-            <li className="nav-item">
+            {/*<li className="nav-item">
               <Link to={"/register"} className="nav-link">
                 Sign Up
               </Link>
-            </li>
+            </li>*/}
           </div>
         )}
       </nav>
