90777b9d298b2db0c86c0df9fb3808a37a4d5101
CoelhoBatfix <panoff.2008@gmail.com>
2023-02-18 00:46:40 +0200

    Added card to macroservices page and switch button in it

--- admin/src/components/Macroservices.tsx
+++ admin/src/components/Macroservices.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import "./styles/macroservices.css"
 
 const Macroservices: React.FC = () => {
+    
+    const OneCard = () => {
+        return(
+        <div>
+            <h1>This is Macroservices</h1>
+            <div className="card mc-cards">
+                <div className="card-body">
+                    <h5 className="card-title">Macroservice #1</h5>
+                    <p className="card-text">This label for small description for this current MC</p>
+                    <div className="form-check form-switch">
+                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+
     return(
-        <h1>This is Macroservices</h1>
+        <>
+            <OneCard />
+        </>
     )
 }
 
