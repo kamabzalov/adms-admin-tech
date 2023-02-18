259d67dc1ec70e497a6db625c0ad59d67bfcbfce
CoelhoBatfix <panoff.2008@gmail.com>
2023-02-19 01:37:11 +0200

    Added users template page
    I made a table for users.
    Here are new features:
    - Table Head component
    - Table Body component

--- admin/src/components/Users.tsx
+++ admin/src/components/Users.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { TableHead } from "./small-components/TableHeads";
+import { UsersTableBody } from "./small-components/TableBody";
+import "./styles/table.css"
 
 const Users: React.FC = () => {
     return(
-        <h1>This is Users</h1>
+        <>
+            <h1>This is Users</h1>
+            <table>
+                <TableHead firstRow="Full Name" secondRow="Role" />
+                <UsersTableBody number="1" fullName="Zeratul" role="Admin" />
+                <UsersTableBody number="2" fullName="Artanis" role="Admin" />
+                <UsersTableBody number="3" fullName="Reynor" role="Admin" />
+            </table>
+        </>
     )
 }
 
