6f8792f2a370ebbdd7e3cbea9a609e6e656a7664
Arsenii Panov <39829327+arsenii10000@users.noreply.github.com>
2023-05-20 01:21:56 +0300

    Added functions for microservices

--- admin/src/services/microservices.service.ts
+++ admin/src/services/microservices.service.ts
@@ -1,12 +1,64 @@
 import axios from "axios";
-import * as AuthService from "./auth.service";
+import microserviceHeader from "./microservices.header";
 
 const API_URL = "http://localhost:6788/api/v1/";
 
 export const listServices = () => {
   return axios.get(API_URL + "services", {
-    headers: {
-      Authorization: "Bearer" + AuthService.getCurrentUser().token,
-    },
+    headers: microserviceHeader(),
   });
+};
+
+export const getServiceState = (id: number) => {
+  return axios.get(API_URL + "services" + "/" + id.toString(), {
+    headers: microserviceHeader(),
+  });
+};
+
+export const getServiceLogs = (id: number) => {
+  return axios.get(API_URL + "services" + "/" + id.toString() + "/logs", {
+    headers: microserviceHeader(),
+  });
+};
+
+export const getServiceAudit = (id: number) => {
+  return axios.get(API_URL + "services" + "/" + id.toString() + "/audit", {
+    headers: microserviceHeader(),
+  });
+};
+
+export const getServiceAllerts = (id: number) => {
+  return axios.get(API_URL + "services" + "/" + id.toString() + "/allerts", {
+    headers: microserviceHeader(),
+  });
+};
+
+export const getServiceCounters = (id: number) => {
+  return axios.get(API_URL + "services" + "/" + id.toString() + "/counters", {
+    headers: microserviceHeader(),
+  });
+};
+
+export const startService = (id: number) => {
+  return axios
+    .post(API_URL + "services" + "/" + id.toString() + "/start", {
+      headers: microserviceHeader(),
+      id,
+    })
+    .catch((err) => {
+      console.error(err);
+      return { data: {} };
+    });
+};
+
+export const stopService = (id: number) => {
+  return axios
+    .post(API_URL + "services" + "/" + id.toString() + "/stop", {
+      headers: microserviceHeader(),
+      id,
+    })
+    .catch((err) => {
+      console.error(err);
+      return { data: {} };
+    });
 };
