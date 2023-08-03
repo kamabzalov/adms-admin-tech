4c46ed64cc183c075a67995a61948e8c5adbf6ad
JSDID <icegouse@gmail.com>
2023-08-03 16:42:58 +0300

    view users card

---
+++ src/components/dashboard/helpers/helpers.tsx
@@ -0,0 +1,193 @@
+import clsx from 'clsx'
+import { useState } from 'react'
+
+export interface IMicroserviceServerData {
+    info: string
+    status: string
+    timestamp: string
+    value?: string
+}
+
+interface ITabValues {
+    activeTab: string
+    tabName: string
+    tabIndex: number
+    tabContent: string
+    checkbox?: boolean
+}
+
+const CustomCheckbox = ({
+    currentValue,
+    id,
+    title,
+}: {
+    currentValue: number
+    id: string
+    title: string
+}) => {
+    const [value, setValue] = useState<number>(currentValue)
+
+    const handleChange = () => {
+        setValue((prevValue: any) => (prevValue === 1 ? 0 : 1))
+    }
+
+    return (
+        <div className='mb-10'>
+            <div className='form-check form-check-custom form-check-solid'>
+                <input
+                    className='form-check-input'
+                    type='checkbox'
+                    value={value}
+                    checked={value === 1}
+                    onChange={handleChange}
+                    id={`checkbox-${id}`}
+                />
+                <label className='form-check-label' htmlFor={`checkbox-${id}`}>
+                    {title}
+                </label>
+            </div>
+        </div>
+    )
+}
+
+const getColumns = (data: IMicroserviceServerData[]) => {
+    const columns = new Set<string>()
+
+    data.forEach((obj: IMicroserviceServerData): void => {
+        Object.keys(obj).forEach((key: string): void => {
+            columns.add(key)
+        })
+    })
+    return [...columns].map((column: string, index: number) => (
+        <th key={`${column}-${index}`}>{column}</th>
+    ))
+}
+
+const getRows = (data: IMicroserviceServerData[]) => {
+    return (
+        <>
+            {data.map((row: IMicroserviceServerData, index: number) => (
+                <tr key={`${data}-${index}`}>
+                    {Object.values(row).map((cell: string) => (
+                        <td key={`${Math.random()}-${cell}-${index}`}>{cell}</td>
+                    ))}
+                </tr>
+            ))}
+        </>
+    )
+}
+
+const renderTable = (data: IMicroserviceServerData[]) => {
+    return (
+        <div className='table-responsive'>
+            <table
+                className={clsx(`table table-row-dashed table-row-gray-300 gy-7`, {
+                    'table-striped': data.length > 5,
+                })}
+            >
+                <thead>
+                    <tr className='fw-bold fs-6 text-gray-800 border-bottom border-gray-200'>
+                        {getColumns(data)}
+                    </tr>
+                </thead>
+                <tbody>{getRows(data)}</tbody>
+            </table>
+        </div>
+    )
+}
+
+const renderList = (data: any, checkbox: boolean = false) => {
+    if (typeof data !== 'object' || data === null) {
+        return (
+            <div>
+                <span className='text-muted d-block fw-semibold mb-6'>{data}</span>
+            </div>
+        )
+    }
+
+    const properties = Object.entries(data)
+
+    return properties.map(([key, value]: [string, any], index: number) => {
+        const title = key.replace(/^[^a-zа-яё]*([a-zа-яё])/i, (letter: string) =>
+            letter.toUpperCase()
+        )
+
+        if (typeof value === 'object' && value !== null) {
+            if (Array.isArray(value)) {
+                return (
+                    <div key={`${key}-${index}`}>
+                        <div className='ms-6'>{renderTable(value)}</div>
+                    </div>
+                )
+            }
+            return (
+                <div key={`${key}-${index}`}>
+                    <span className='text-dark fw-bold fs-6'>{title}:</span>
+                    <div className='ms-6'>{renderList(value)}</div>
+                </div>
+            )
+        } else {
+            const activeCheckbox = checkbox && (Number(value) === 0 || Number(value) === 1)
+            return (
+                <div key={`${key}-${index}`}>
+                    {activeCheckbox ? (
+                        <CustomCheckbox currentValue={value} id={key} title={title} />
+                    ) : (
+                        <>
+                            <span className='text-dark fw-bold fs-6'>{title}:</span>
+                            <span className='text-muted d-block fw-semibold mb-6'>{value}</span>
+                        </>
+                    )}
+                </div>
+            )
+        }
+    })
+}
+
+const dataWrapper = (title: string, data: string, checkbox: boolean = false) => {
+    const parsedData = JSON.parse(data)
+
+    const renderContent = () => {
+        if (typeof parsedData === 'object' && !Array.isArray(parsedData)) {
+            return renderList(parsedData, checkbox)
+        } else {
+            return renderTable(parsedData)
+        }
+    }
+
+    return (
+        <>
+            <div className='card card-custom mb-6'>
+                <div className='card-header'>
+                    <h3 className='card-title fw-bolder text-dark'>{title} as JSON view</h3>
+                </div>
+                <div className='card-body'>
+                    <pre className='fs-4'>{data}</pre>
+                </div>
+            </div>
+            <div className='card card-custom '>
+                <div className='card-header'>
+                    <h3 className='card-title fw-bolder text-dark'>{title}</h3>
+                </div>
+                <div className='card-body'>
+                    {parsedData ? renderContent() : 'No data available'}
+                </div>
+            </div>
+        </>
+    )
+}
+export const TabContent = ({
+    activeTab,
+    tabName,
+    tabIndex,
+    tabContent,
+    checkbox = false,
+}: ITabValues) => (
+    <div
+        className={clsx('tab-pane', { active: activeTab === tabName })}
+        id={`kt_tab_pane_${tabIndex}`}
+        role='tabpanel'
+    >
+        {tabContent && dataWrapper(tabName, tabContent, checkbox)}
+    </div>
+)
--- src/components/dashboard/microservices/MicroserviceCard.tsx
+++ src/components/dashboard/microservices/MicroserviceCard.tsx
@@ -9,13 +9,7 @@ import {
     Microservice,
 } from './service'
 import clsx from 'clsx'
-
-interface IServerData {
-    info: string
-    status: string
-    timestamp: string
-    value?: string
-}
+import { TabContent } from '../helpers/helpers'
 
 enum Tabs {
     State = 'State',
@@ -25,91 +19,14 @@ enum Tabs {
     Counters = 'Counters',
 }
 
-interface ITabValues {
-    tabName: string
-    tabIndex: number
-    tabContent: string
-}
-
-const getColumns = (data: string): string[] => {
-    const parsedData = JSON.parse(data)
-    const columns = new Set()
-
-    parsedData.forEach((obj: IServerData) => {
-        Object.keys(obj).forEach((key: string): void => {
-            columns.add(key)
-        })
-    })
-    return [...columns] as string[]
-}
-
-const getRows = (data: string) => {
-    const parsedData = JSON.parse(data)
-    return (
-        <>
-            {parsedData.map((row: IServerData, index: number) => (
-                <tr key={index}>
-                    {Object.values(row).map((cell) => (
-                        <td key={cell}>{cell}</td>
-                    ))}
-                </tr>
-            ))}
-        </>
-    )
-}
-
-const renderTable = (data: string) => (
-    <div className='table-responsive'>
-        <table className='table table-row-dashed table-row-gray-300 gy-7'>
-            <thead>
-                <tr className='fw-bold fs-6 text-gray-800 border-bottom border-gray-200'>
-                    {getColumns(data).map((column: string) => (
-                        <th key={column}>{column}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>{getRows(data)}</tbody>
-        </table>
-    </div>
-)
-
-const dataWrapper = (title: string, data: string) => (
-    <>
-        <div className='card card-custom mb-6'>
-            <div className='card-header'>
-                <h3 className='card-title fw-bolder text-dark'>{title} as JSON view</h3>
-            </div>
-            <div className='card-body'>
-                <pre className='fs-4'>{data}</pre>
-            </div>
-        </div>
-        <div className='card card-custom '>
-            <div className='card-header'>
-                <h3 className='card-title fw-bolder text-dark'>{title}</h3>
-            </div>
-            <div className='card-body'>{renderTable(data)}</div>
-        </div>
-    </>
-)
-
 export function MicroserviceCard() {
     const { uid } = useParams()
-    const [tab, setTab] = useState('State')
+    const [activeTab, setActiveTab] = useState('State')
     const [logs, setLogs] = useState<string>('')
     const [audit, setAudit] = useState<string>('')
     const [alerts, setAlerts] = useState<string>('')
     const [counters, setCounters] = useState<string>('')
     const [microserviceData, setMicroservice] = useState<Microservice | null>(null)
-
-    const TabContent = ({ tabName, tabIndex, tabContent }: ITabValues) => (
-        <div
-            className={clsx('tab-pane', { active: tab === tabName })}
-            id={`kt_tab_pane_${tabIndex}`}
-            role='tabpanel'
-        >
-            {tabContent && dataWrapper(tabName, tabContent)}
-        </div>
-    )
 
     useEffect(() => {
         if (uid) {
@@ -153,9 +70,9 @@ export function MicroserviceCard() {
                             <li className='nav-item'>
                                 <button
                                     className={clsx(`nav-link text-active-primary cursor-pointer`, {
-                                        active: tab === Tabs.State,
+                                        active: activeTab === Tabs.State,
                                     })}
-                                    onClick={() => setTab(Tabs.State)}
+                                    onClick={() => setActiveTab(Tabs.State)}
                                     role='tab'
                                 >
                                     {Tabs.State}
@@ -164,9 +81,9 @@ export function MicroserviceCard() {
                             <li className='nav-item'>
                                 <button
                                     className={clsx(`nav-link text-active-primary cursor-pointer`, {
-                                        active: tab === Tabs.Logs,
+                                        active: activeTab === Tabs.Logs,
                                     })}
-                                    onClick={() => setTab(Tabs.Logs)}
+                                    onClick={() => setActiveTab(Tabs.Logs)}
                                     role='tab'
                                 >
                                     {Tabs.Logs}
@@ -175,9 +92,9 @@ export function MicroserviceCard() {
                             <li className='nav-item'>
                                 <button
                                     className={clsx(`nav-link text-active-primary cursor-pointer`, {
-                                        active: tab === Tabs.Audit,
+                                        active: activeTab === Tabs.Audit,
                                     })}
-                                    onClick={() => setTab(Tabs.Audit)}
+                                    onClick={() => setActiveTab(Tabs.Audit)}
                                     role='tab'
                                 >
                                     {Tabs.Audit}
@@ -186,9 +103,9 @@ export function MicroserviceCard() {
                             <li className='nav-item'>
                                 <button
                                     className={clsx(`nav-link text-active-primary cursor-pointer`, {
-                                        active: tab === Tabs.Alerts,
+                                        active: activeTab === Tabs.Alerts,
                                     })}
-                                    onClick={() => setTab(Tabs.Alerts)}
+                                    onClick={() => setActiveTab(Tabs.Alerts)}
                                     role='tab'
                                 >
                                     {Tabs.Alerts}
@@ -197,9 +114,9 @@ export function MicroserviceCard() {
                             <li className='nav-item'>
                                 <button
                                     className={clsx(`nav-link cursor-pointer`, {
-                                        active: tab === Tabs.Counters,
+                                        active: activeTab === Tabs.Counters,
                                     })}
-                                    onClick={() => setTab(Tabs.Counters)}
+                                    onClick={() => setActiveTab(Tabs.Counters)}
                                     role='tab'
                                 >
                                     {Tabs.Counters}
@@ -211,7 +128,7 @@ export function MicroserviceCard() {
 
                 <div className='tab-content' id='myTabContent'>
                     <div
-                        className={clsx('tab-pane', { active: tab === Tabs.State })}
+                        className={clsx('tab-pane', { active: activeTab === Tabs.State })}
                         id='kt_tab_pane_1'
                         role='tabpanel'
                     >
@@ -317,10 +234,30 @@ export function MicroserviceCard() {
                             </div>
                         </div>
                     </div>
-                    <TabContent tabName={Tabs.Logs} tabIndex={2} tabContent={logs} />
-                    <TabContent tabName={Tabs.Audit} tabIndex={3} tabContent={audit} />
-                    <TabContent tabName={Tabs.Alerts} tabIndex={4} tabContent={alerts} />
-                    <TabContent tabName={Tabs.Counters} tabIndex={5} tabContent={counters} />
+                    <TabContent
+                        activeTab={activeTab}
+                        tabName={Tabs.Logs}
+                        tabIndex={2}
+                        tabContent={logs}
+                    />
+                    <TabContent
+                        activeTab={activeTab}
+                        tabName={Tabs.Audit}
+                        tabIndex={3}
+                        tabContent={audit}
+                    />
+                    <TabContent
+                        activeTab={activeTab}
+                        tabName={Tabs.Alerts}
+                        tabIndex={4}
+                        tabContent={alerts}
+                    />
+                    <TabContent
+                        activeTab={activeTab}
+                        tabName={Tabs.Counters}
+                        tabIndex={5}
+                        tabContent={counters}
+                    />
                 </div>
             </div>
         </div>
