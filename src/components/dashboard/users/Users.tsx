09b6090eacee09179e75b09af460d3b59989464d
JSDID <icegouse@gmail.com>
2023-08-08 22:01:53 +0300

    refactor: tab navigation

--- src/components/dashboard/users/Users.tsx
+++ src/components/dashboard/users/Users.tsx
@@ -1,9 +1,21 @@
 import { useEffect, useState } from 'react'
 import { deleteUser, getDeletedUsers, getUsers, undeleteUser, User } from './user.service'
 import { Link } from 'react-router-dom'
+import clsx from 'clsx'
+import { TabNavigate } from '../helpers/helpers'
+
+enum UsersTabs {
+    Users = "Users",
+    DeletedUsers = "Deleted users"
+}
+
+
+const usersTabsArray: string[] = Object.values(UsersTabs) as string[]
 
 export default function Users() {
     const [users, setUsers] = useState<User[]>([])
+
+    const [activeTab, setActiveTab] = useState('Users')
     const [deletedUsers, setDeletedUsers] = useState<User[]>([])
     const [loaded, setLoaded] = useState<boolean>(false)
 
@@ -50,92 +62,118 @@ export default function Users() {
         })
     }
 
+    const handleTabClick = (tab: string) => {
+        setActiveTab(tab);
+    };
+
     return (
         <>
-            <div className='mb-10'>
-                <h1 className='mb-5'>Users</h1>
-                <div className='card'>
-                    <div className='card-body'>
-                        <div className='table-responsive'>
-                            <table
-                                id='kt_table_users'
-                                className='table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer'
-                            >
-                                <thead>
-                                    <tr className='text-start text-muted fw-bolder fs-7 text-uppercase gs-0'>
-                                        <th>User name</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody className='text-gray-600 fw-bold'>
-                                    {users.map((user) => {
-                                        return (
-                                            <tr key={user.useruid}>
-                                                <td>
-                                                    <Link
-                                                        to={`${user.useruid}`}
-                                                        className='text-gray-800 text-hover-primary mb-1'
-                                                    >
-                                                        {user.username}
-                                                    </Link>
-                                                </td>
-                                                <td>
-                                                    <button
-                                                        className='btn btn-danger'
-                                                        onClick={() => moveToTrash(user.useruid)}
-                                                    >
-                                                        Delete user
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </table>
+            <div className="card">
+                <div className='card-header d-flex flex-column justify-content-end pb-0'>
+                    <ul className='nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder flex-nowrap'>
+                        {
+                            usersTabsArray.map(tab => <TabNavigate key={tab} activeTab={activeTab} tab={tab} onTabClick={handleTabClick} />)
+                        }
+                    </ul>
+                </div>
+
+                <div className='tab-content' id='myTabContentInner'>
+                    <div
+                        className={clsx('tab-pane vw-90 mx-auto', {
+                            active: activeTab === UsersTabs.Users,
+                        })}
+                        id={`kt_tab_pane_${1}`}
+                        role='tabpanel'
+                    >
+                        <div className='card-body'>
+                            <div className='table-responsive'>
+                                <table
+                                    id='kt_table_users'
+                                    className='table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer'
+                                >
+                                    <thead>
+                                        <tr className='text-start text-muted fw-bolder fs-7 text-uppercase gs-0'>
+                                            <th>User name</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className='text-gray-600 fw-bold'>
+                                        {users.map((user) => {
+                                            return (
+                                                <tr key={user.useruid}>
+                                                    <td>
+                                                        <Link
+                                                            to={`${user.useruid}`}
+                                                            className='text-gray-800 text-hover-primary mb-1'
+                                                        >
+                                                            {user.username}
+                                                        </Link>
+                                                    </td>
+                                                    <td>
+                                                        <button
+                                                            className='btn btn-danger'
+                                                            onClick={() => moveToTrash(user.useruid)}
+                                                        >
+                                                            Delete user
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <h1 className='mb-5'>Deleted Users</h1>
-            <div className='card'>
-                <div className='card-body'>
-                    <div className='table-responsive'>
-                        <table
-                            id='kt_table_users'
-                            className='table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer'
-                        >
-                            <thead>
-                                <tr className='text-start text-muted fw-bolder fs-7 text-uppercase gs-0'>
-                                    <th>User name</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody className='text-gray-600 fw-bold'>
-                                {deletedUsers.map((user) => {
-                                    return (
-                                        <tr key={user.useruid}>
-                                            <td>
-                                                <Link
-                                                    to={`${user.useruid}`}
-                                                    className='text-gray-800 text-hover-primary mb-1'
-                                                >
-                                                    {user.username}
-                                                </Link>
-                                            </td>
-                                            <td>
-                                                <button
-                                                    className='btn btn-success'
-                                                    onClick={() => restoreUser(user.useruid)}
-                                                >
-                                                    Restore user
-                                                </button>
-                                            </td>
+                <div className='tab-content' id='myTabContentInner'>
+                    <div
+                        className={clsx('tab-pane vw-90 mx-auto', {
+                            active: activeTab === UsersTabs.DeletedUsers,
+                        })}
+                        id={`kt_tab_pane_${2}`}
+                        role='tabpanel'
+                    >
+                        <div className='card-body'>
+                            <div className='table-responsive'>
+                                <table
+                                    id='kt_table_users'
+                                    className='table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer'
+                                >
+                                    <thead>
+                                        <tr className='text-start text-muted fw-bolder fs-7 text-uppercase gs-0'>
+                                            <th>User name</th>
+                                            <th>Actions</th>
                                         </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
+                                    </thead>
+                                    <tbody className='text-gray-600 fw-bold'>
+                                        {deletedUsers.map((user) => {
+                                            return (
+                                                <tr key={user.useruid}>
+                                                    <td>
+                                                        <Link
+                                                            to={`${user.useruid}`}
+                                                            className='text-gray-800 text-hover-primary mb-1'
+                                                        >
+                                                            {user.username}
+                                                        </Link>
+                                                    </td>
+                                                    <td>
+                                                        <button
+                                                            className='btn btn-success'
+                                                            onClick={() => restoreUser(user.useruid)}
+                                                        >
+                                                            Restore user
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
