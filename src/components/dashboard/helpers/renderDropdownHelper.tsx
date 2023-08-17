3b5acfb6796f8553ab931f1b4abb355dbf99250e
JSDID <icegouse@gmail.com>
2023-08-17 13:04:29 +0300

    dropdown

--- src/components/dashboard/helpers/renderDropdownHelper.tsx
+++ src/components/dashboard/helpers/renderDropdownHelper.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { FC, useState, useEffect, useRef } from 'react'
-import clsx from 'clsx'
+import { FC } from 'react'
 
 interface PropsItems {
     menuItemName: string
@@ -13,51 +12,22 @@ interface Props {
 }
 
 export const CustomDropdown: FC<Props> = ({ title, items }) => {
-    const [dropdownOpen, setDropdownOpen] = useState(false)
-    const dropdownRef = useRef<HTMLDivElement | null>(null)
-
-    const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen)
-    }
-
-    useEffect(() => {
-        const closeDropdown = (event: MouseEvent) => {
-            if (
-                dropdownRef.current &&
-                !(
-                    dropdownRef.current instanceof Node &&
-                    dropdownRef.current.contains(event.target as Node)
-                )
-            ) {
-                setDropdownOpen(false)
-            }
-        }
-
-        document.addEventListener('click', closeDropdown)
-
-        return () => {
-            document.removeEventListener('click', closeDropdown)
-        }
-    }, [])
-
     return (
-        <div ref={dropdownRef} className={clsx('dropdown', { show: dropdownOpen })}>
-            <button
-                className='btn btn-light btn-active-light-primary btn-sm fw-bold'
-                onClick={toggleDropdown}
+        <div className='m-0'>
+            <a
+                href='#'
+                className='btn btn-light btn-active-light-primary btn-sm'
+                data-kt-menu-trigger='click'
+                data-kt-menu-placement='bottom-end'
             >
-                {title}
-                <i className='ki-duotone ki-down fs-5 m-0'></i>
-            </button>
+                Actions<i className='ki-duotone ki-down fs-5 m-0'></i>
+            </a>
             <div
-                className={clsx(
-                    'dropdown-menu menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4',
-                    { show: dropdownOpen }
-                )}
-                onClick={(e) => e.stopPropagation()}
+                className='menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4'
+                data-kt-menu='true'
             >
-                {items.map(({ menuItemName, menuItemAction }, idx) => (
-                    <div key={menuItemName + idx} className='menu-item px-3 cursor-pointer'>
+                {items.map(({ menuItemName, menuItemAction }) => (
+                    <div key={menuItemName} className='menu-item px-3'>
                         <a className='menu-link px-3' onClick={menuItemAction}>
                             {menuItemName}
                         </a>
