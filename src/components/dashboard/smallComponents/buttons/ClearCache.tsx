520728949ae82d2dd325d12e9e3144a13eae441a
Oleksandr <102165823+JSDID@users.noreply.github.com>
2023-08-22 20:58:35 +0300

    Update ClearCache.tsx

--- src/components/dashboard/smallComponents/buttons/ClearCache.tsx
+++ src/components/dashboard/smallComponents/buttons/ClearCache.tsx
@@ -2,7 +2,7 @@ import { updateAll } from '../../users/user.service'
 
 const ClearCache = () => (
     <div className='fixed-bottom d-flex justify-content-end m-4'>
-        <button className='btn btn-primary btn-icon px-14 fs-8' onClick={() => updateAll()}>
+        <button className='btn btn-primary btn-icon px-14 fs-8' onClick={() => clearCach()}>
             <i className='ki-duotone ki-trash fs-1'>
                 <span className='path1'></span>
                 <span className='path2'></span>
