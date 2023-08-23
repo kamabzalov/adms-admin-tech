460ab4061fe7119179c0b5da8b520cf7e3b139ab
JSDID <icegouse@gmail.com>
2023-08-23 08:58:30 +0300

    add: clear cache button

--- src/components/dashboard/users/user.service.ts
+++ src/components/dashboard/users/user.service.ts
@@ -23,6 +23,57 @@ export const user = (uid: string | number = 0, loginname: string, loginpassword:
     )
 }
 
+
+export const clearCache = () => {
+    return axios
+        .get<string[]>(`${API_URL}user/updateall`, {
+            headers: { Authorization: `Bearer ${getToken()}` },
+        })
+        .then((response) => response.data)
+}
+
+export const createUser = (loginname: string, loginpassword: string) => {
+    return axios.post(
+        API_URL + 'user/' + 0 + '/user',
+        { loginname: loginname, loginpassword: loginpassword },
+        {
+            headers: { Authorization: `Bearer ${getToken()}` },
+        }
+    )
+}
+
+export const copyUser = (srcuid: string) => {
+    return axios
+        .post<ActionStatus>(
+            `${API_URL}user/${srcuid}/copyuser`,
+            {},
+            {
+                headers: { Authorization: `Bearer ${getToken()}` },
+            }
+        )
+        .then((response) => response.data)
+}
+
+export const updateUser = (uid: string, loginname: string, loginpassword: string) => {
+    return axios.post(
+        API_URL + 'user/' + uid + '/user',
+        { loginname: loginname, loginpassword: loginpassword },
+        {
+            headers: { Authorization: `Bearer ${getToken()}` },
+        }
+    )
+}
+
+export const setUserOptionalData = (uid: string, data: any) => {
+    return axios.post(
+        API_URL + 'user/' + uid + '/set',
+        { ...data },
+        {
+            headers: { Authorization: `Bearer ${getToken()}` },
+        }
+    )
+}
+
 export const getUsers = () => {
     return axios
         .get<User[]>(`${API_URL}user/0/list`, {
@@ -63,6 +114,16 @@ export const undeleteUser = (uid: string) => {
         .then((response) => response.data)
 }
 
+export const setUserPermissions = (uid: string, data: any) => {
+    return axios.post(
+        API_URL + 'user/' + uid + '/permissions',
+        { ...data },
+        {
+            headers: { Authorization: `Bearer ${getToken()}` },
+        }
+    )
+}
+
 export const getUserPermissions = (uid: string) => {
     return axios
         .get<string>(`${API_URL}user/${uid}/permissions`, {
@@ -87,12 +148,28 @@ export const getUserLocations = (uid: string) => {
         .then((response) => response.data)
 }
 
+export const setUserProfile = (uid: string, profile: any) => {
+    return axios.post(
+        API_URL + 'user/' + uid + '/profile',
+        { ...profile },
+        {
+            headers: { Authorization: `Bearer ${getToken()}` },
+        }
+    )
+}
+
 export const getUserProfile = (uid: string) => {
     return axios
         .get<string>(`${API_URL}user/${uid}/profile`, {
             headers: { Authorization: `Bearer ${getToken()}` },
         })
         .then((response) => response.data)
+}
+
+export const setUserSettings = (uid: string, data: any) => {
+    return axios.post(API_URL + 'user/' + uid + '/settings', data, {
+        headers: { Authorization: `Bearer ${getToken()}` },
+    })
 }
 
 export const getUserSettings = (uid: string) => {
@@ -103,12 +180,30 @@ export const getUserSettings = (uid: string) => {
         .then((response) => response.data)
 }
 
+export const checkToken = (token: string) => {
+    return axios.get(API_URL + 'user/' + token + '/token', {
+        headers: { Authorization: `Bearer ${getToken()}` },
+    })
+}
+
 export const listUserSessions = (uid: string) => {
     return axios
         .get<string>(`${API_URL}user/${uid}/sessions`, {
             headers: { Authorization: `Bearer ${getToken()}` },
         })
         .then((response) => response.data)
+}
+
+export const killSession = (id: number) => {
+    return axios.post(API_URL + 'user/' + id.toString() + '/session', {
+        headers: { Authorization: `Bearer ${getToken()}` },
+    })
+}
+
+export const checkSession = (uid: string) => {
+    return axios.get(API_URL + 'user/' + uid + '/session', {
+        headers: { Authorization: `Bearer ${getToken()}` },
+    })
 }
 
 export const listUserLogins = (uid: string) => {
@@ -154,14 +249,6 @@ export const getAllUIPermissions = (uid: string) => {
 export const getAllUITypes = (uid: string) => {
     return axios
         .get<string>(`${API_URL}user/${uid}/listusertypes`, {
-            headers: { Authorization: `Bearer ${getToken()}` },
-        })
-        .then((response) => response.data)
-}
-
-export const clearCache = () => {
-    return axios
-        .get<string[]>(`${API_URL}user/updateall`, {
             headers: { Authorization: `Bearer ${getToken()}` },
         })
         .then((response) => response.data)
