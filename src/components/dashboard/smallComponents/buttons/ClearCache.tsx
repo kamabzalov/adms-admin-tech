805c978bc682d1fd6b69c5c28bb441fae3a7042f
JSDID <icegouse@gmail.com>
2023-08-24 17:00:25 +0300

    Add user modal and import

--- src/components/dashboard/smallComponents/buttons/ClearCache.tsx
+++ src/components/dashboard/smallComponents/buttons/ClearCache.tsx
@@ -1,8 +1,15 @@
-import { clearCache } from '../../users/user.service'
+import { updateAll } from '../../users/user.service'
 
 const ClearCache = () => (
-    <div className='fixed-bottom d-flex justify-content-end m-3'>
-        <button className='btn btn-warning font-weight-bold mr-2' onClick={() => clearCache()}>
+    <div className='fixed-bottom d-flex justify-content-end m-4'>
+        <button className='btn btn-primary btn-icon px-14 fs-8' onClick={() => updateAll()}>
+            <i className='ki-duotone ki-trash fs-1'>
+                <span className='path1'></span>
+                <span className='path2'></span>
+                <span className='path3'></span>
+                <span className='path4'></span>
+                <span className='path5'></span>
+            </i>
             Clear server cache
         </button>
     </div>
