b1223d17dca1c1c934a25db71db027a8e9699b89
JSDID <icegouse@gmail.com>
2023-08-24 16:45:11 +0300

    Add user modal

--- src/components/smallComponents/ClearCache.tsx
+++ src/components/smallComponents/ClearCache.tsx
@@ -1,8 +1,8 @@
-import { clearCache } from '../dashboard/users/user.service'
+import { updateAll } from '../dashboard/users/user.service'
 
 export const ClearCache = () => (
     <div className='fixed-bottom d-flex justify-content-end m-4'>
-        <button className='btn btn-warning' onClick={() => clearCache()}>
+        <button className='btn btn-warning' onClick={() => updateAll()}>
             Clear server cache
         </button>
     </div>
