6c8bc7f8423b083f511b97f2a397713033366bdd
JSDID <icegouse@gmail.com>
2023-09-02 16:09:57 +0300

    Add icons in main menu

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -22,16 +22,19 @@ export function DashboardHeader() {
                         <div className='menu menu-rounded menu-column menu-lg-row my-5 my-lg-0 align-items-stretch fw-semibold px 2 px-lg-0'>
                             <div className='menu-item me-lg-1'>
                                 <Link className='menu-link py-3' to={'/dashboard'}>
+                                    <i className='ki-outline ki-gear fs-2 m-2'></i>
                                     <span className='menu-title'>Microservices</span>
                                 </Link>
                             </div>
                             <div className='menu-item me-lg-1'>
                                 <Link className='menu-link py-3' to={'users'}>
+                                    <i className='ki-outline ki-user-tick fs-2 m-2'></i>
                                     <span className='menu-title'>Users</span>
                                 </Link>
                             </div>
                             <div className='menu-item me-lg-1'>
                                 <span onClick={() => signOut()} className='menu-link'>
+                                    <i className='ki-outline ki-exit-right fs-2 m-2'></i>
                                     <span className='menu-title'>Log out</span>
                                 </span>
                             </div>
