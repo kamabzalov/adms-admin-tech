861ded63c73d060133a6a0f37e0fe699bc6e084c
JSDID <icegouse@gmail.com>
2023-09-21 14:42:24 +0300

    Added sorting, pagination and search in the user table

---
+++ src/components/dashboard/users/table/columns/CustomHeaderColumn.tsx
@@ -0,0 +1,18 @@
+// @ts-nocheck
+import { FC } from 'react';
+import { ColumnInstance } from 'react-table';
+import { User } from '../../core/_models';
+
+type Props = {
+    column: ColumnInstance<User>;
+};
+
+export const CustomHeaderColumn: FC<Props> = ({ column }) => (
+    <>
+        {column.Header && typeof column.Header === 'string' ? (
+            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+        ) : (
+            column.render('Header')
+        )}
+    </>
+);
