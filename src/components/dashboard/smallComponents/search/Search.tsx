bd8cb48de13bdea1d6c6a1cde2ff8ddef9e72644
JSDID <icegouse@gmail.com>
2023-10-30 18:12:28 +0200

    Refactoring

--- src/components/dashboard/smallComponents/search/Search.tsx
+++ src/components/dashboard/smallComponents/search/Search.tsx
@@ -7,11 +7,9 @@ export const UsersListSearchComponent = () => {
     const { state, updateState } = useQueryRequest();
     const [searchTerm, setSearchTerm] = useState<string>('');
     const [isSearchUnchanged, setIsSearchUnchanged] = useState<boolean>(true);
-    const [isSearching, setIsSearching] = useState<boolean>(false);
 
     useEffect(() => {
         setIsSearchUnchanged(searchTerm === state.search);
-        !searchTerm && setIsSearching(false);
     }, [searchTerm, state.search]);
 
     const handleSearch = (): void => {
@@ -19,17 +17,13 @@ export const UsersListSearchComponent = () => {
             setIsSearchUnchanged(true);
             try {
                 updateState({ ...state, search: searchTerm, currentpage: 0 });
-                setIsSearching(true);
-            } catch (error) {
-                setIsSearching(false);
-            }
+            } catch (error) {}
         }
     };
 
     const handleClear = (): void => {
         setSearchTerm('');
         updateState(initialQueryState);
-        setIsSearching(false);
     };
 
     const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>): void => {
@@ -40,34 +34,29 @@ export const UsersListSearchComponent = () => {
     };
 
     return (
-        <div className='d-flex align-items-center my-1'>
-            <div className='input-group position-relative'>
-                <input
-                    type='text'
-                    data-kt-user-table-filter='search'
-                    className='form-control form-control-solid w-250px'
-                    placeholder='Search user'
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    onKeyUp={handleKeyPress}
-                />
-                {isSearching ? (
-                    <button
-                        className={clsx('btn position-absolute end-0 me-14')}
-                        onClick={handleClear}
-                    >
-                        <i className='ki-outline ki-cross fs-2'></i>
-                    </button>
-                ) : null}
-                <button
-                    className={clsx('btn btn-primary', {
-                        disabled: isSearchUnchanged,
-                    })}
-                    onClick={handleSearch}
-                >
-                    <i className='ki-outline ki-magnifier fs-2'></i>
+        <div className='input-group w-350px'>
+            <input
+                type='text'
+                data-kt-user-table-filter='search'
+                className='form-control form-control-solid pe-4'
+                placeholder='Search user'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyUp={handleKeyPress}
+            />
+            {searchTerm && (
+                <button className={clsx('btn btn-light input-group-append')} onClick={handleClear}>
+                    <i className='ki-outline ki-cross fs-2'></i>
                 </button>
-            </div>
+            )}
+            <button
+                className={clsx('btn btn-primary', {
+                    disabled: isSearchUnchanged,
+                })}
+                onClick={handleSearch}
+            >
+                <i className='ki-outline ki-magnifier fs-2'></i>
+            </button>
         </div>
     );
 };
