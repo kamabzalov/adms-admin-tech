9741b2bb2d012c05d83c789ef01a6aed67cf073f
JSDID <icegouse@gmail.com>
2023-11-23 20:49:04 +0200

    Refactoring menu

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -55,7 +55,7 @@ export function DashboardHeader() {
                                     className='menu-link py-3 text-hover-primary'
                                     to={'/dashboard'}
                                 >
-                                    <i className='ki-outline ki-gear fs-2 m-2'></i>
+                                    <i className='ki-outline ki-wrench fs-2 m-2'></i>
                                     <span className='menu-title'>Microservices</span>
                                 </Link>
                             </div>
@@ -65,53 +65,53 @@ export function DashboardHeader() {
                                     <span className='menu-title w-0'>Users</span>
                                 </Link>
                             </div>
-                            <div className='d-flex align-items-center ms-auto gap-6'>
-                                <div className='menu-item me-lg-1'>
-                                    <CustomDropdown
-                                        title={'Common'}
-                                        weight={250}
-                                        items={[
-                                            {
-                                                menuItemName: 'Data import',
-                                                icon: 'data',
-                                                menuItemAction: () => navigate('data-import'),
-                                            },
-                                            {
-                                                menuItemName: 'Templates for reports',
-                                                icon: 'calendar-edit',
-                                                menuItemAction: () => navigate('template-reports'),
-                                            },
-                                            {
-                                                menuItemName: 'Templates for printed forms',
-                                                icon: 'printer',
-                                                menuItemAction: () => navigate('template-printed'),
-                                            },
-                                        ]}
-                                    />
-                                </div>
-                                <div className='menu-item me-lg-1'>
-                                    <CustomDropdown
-                                        title={loginname}
-                                        weight={200}
-                                        items={[
-                                            {
-                                                menuItemName: `${loginname} card`,
-                                                icon: 'user-tick',
-                                                menuItemAction: () => handleUserCardOpen(),
-                                            },
-                                            {
-                                                menuItemName: 'Change password',
-                                                icon: 'lock-2',
-                                                menuItemAction: () => handleEditUserModalOpen(),
-                                            },
-                                            {
-                                                menuItemName: 'Log out',
-                                                icon: 'exit-right',
-                                                menuItemAction: () => signOut(),
-                                            },
-                                        ]}
-                                    />
-                                </div>
+                            <div className='menu-item me-lg-1'>
+                                <CustomDropdown
+                                    title={'Common'}
+                                    background='none'
+                                    iconBefore='gear'
+                                    weight={250}
+                                    items={[
+                                        {
+                                            menuItemName: 'Data import',
+                                            icon: 'data',
+                                            menuItemAction: () => navigate('data-import'),
+                                        },
+                                        {
+                                            menuItemName: 'Templates for reports',
+                                            icon: 'calendar-edit',
+                                            menuItemAction: () => navigate('template-reports'),
+                                        },
+                                        {
+                                            menuItemName: 'Templates for printed forms',
+                                            icon: 'printer',
+                                            menuItemAction: () => navigate('template-printed'),
+                                        },
+                                    ]}
+                                />
+                            </div>
+                            <div className='menu-item me-lg-1 ms-auto'>
+                                <CustomDropdown
+                                    title={loginname}
+                                    weight={200}
+                                    items={[
+                                        {
+                                            menuItemName: `${loginname} card`,
+                                            icon: 'user-tick',
+                                            menuItemAction: () => handleUserCardOpen(),
+                                        },
+                                        {
+                                            menuItemName: 'Change password',
+                                            icon: 'lock-2',
+                                            menuItemAction: () => handleEditUserModalOpen(),
+                                        },
+                                        {
+                                            menuItemName: 'Log out',
+                                            icon: 'exit-right',
+                                            menuItemAction: () => signOut(),
+                                        },
+                                    ]}
+                                />
                             </div>
                         </div>
                     </div>
